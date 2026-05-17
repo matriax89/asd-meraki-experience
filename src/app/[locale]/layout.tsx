ad089c653d389672cbe09d2a694d40667e447d01
Matias <matias.lisio@MBP-di-Matias.homenet.telecomitalia.it>
2026-05-17 21:44:06 +0200

    fix(pixel): move Meta Pixel Script to body - afterInteractive does not work inside head

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -131,25 +131,6 @@ export default async function LocaleLayout({
         )}
         
         {integrations?.facebook_pixel_id && (
-          <>
-            <Script
-              id="fb-pixel"
-              strategy="afterInteractive"
-              dangerouslySetInnerHTML={{
-                __html: `
-                  !function(f,b,e,v,n,t,s)
-                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-                  n.queue=[];t=b.createElement(e);t.async=!0;
-                  t.src=v;s=b.getElementsByTagName(e)[0];
-                  s.parentNode.insertBefore(t,s)}(window, document,'script',
-                  'https://connect.facebook.net/en_US/fbevents.js');
-                  fbq('init', '${integrations.facebook_pixel_id}');
-                  fbq('track', 'PageView');
-                `,
-              }}
-            />
             <noscript>
               <img
                 height="1"
@@ -159,10 +140,29 @@ export default async function LocaleLayout({
                 alt=""
               />
             </noscript>
-          </>
         )}
       </head>
       <body className="min-h-full flex flex-col bg-background text-foreground">
+        {integrations?.facebook_pixel_id && (
+          <Script
+            id="fb-pixel"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${integrations.facebook_pixel_id}');
+                fbq('track', 'PageView');
+              `,
+            }}
+          />
+        )}
         <NextIntlClientProvider messages={messages}>
           <ModalProvider>
             {children}
