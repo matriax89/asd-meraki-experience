9ebe18aa711f420c6cf955e1bda61e50f42e90fa
Matias <matias.lisio@MBP-di-Matias.homenet.telecomitalia.it>
2026-05-17 21:30:14 +0200

    fix(seo): use next/script for facebook pixel to ensure it fires correctly

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,6 +4,7 @@ import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
+import Script from "next/script";
 import "../globals.css";
 
 const inter = Inter({
@@ -131,7 +132,9 @@ export default async function LocaleLayout({
         
         {integrations?.facebook_pixel_id && (
           <>
-            <script
+            <Script
+              id="fb-pixel"
+              strategy="afterInteractive"
               dangerouslySetInnerHTML={{
                 __html: `
                   !function(f,b,e,v,n,t,s)
